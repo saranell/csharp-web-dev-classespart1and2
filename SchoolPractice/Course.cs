cbc268ef6ae118a596a3ef8db1006296edcc64ad
saranell <76405224+saranell@users.noreply.github.com>
2022-08-17 12:05:27 -0500

    Created Course & Teacher classes

---
+++ SchoolPractice/Course.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SchoolPractice
+{
+    public class Course
+    {   
+        private string Subject { get; set; }
+        private string Teacher { get; set; }
+        private List<Student> enrolledStudents;
+        }
+    }
+
---
+++ SchoolPractice/Teacher.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace SchoolPractice
+{
+    public class Teacher
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Subject { get; set; }
+        public int YearsTeaching { get; set; }
+
+    }
+}
+
