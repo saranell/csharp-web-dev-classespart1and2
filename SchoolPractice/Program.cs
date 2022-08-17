ee51ce975c05d97d8e828a72a0e1044801063332
saranell <76405224+saranell@users.noreply.github.com>
2022-08-17 11:56:52 -0500

    Exercises done

--- SchoolPractice/Program.cs
+++ SchoolPractice/Program.cs
@@ -7,6 +7,9 @@ namespace SchoolPractice
         public static void Main(string[] args)
         {
             // TODO: Instantiate your objects and test your exercise solutions with print statements here.
+            // Instantiate the Student class using yourself as the student. For the NumberOfCredits give yourself 1 and a GPA of 4.0.
+            Student sara = new Student("Sara", 1, 1, 4.0);
+            Console.WriteLine(sara.Name + " is a student!");
         }
     }
 }
