9cfc380f4e909fd2890e44e880fc91f6b5de496c
saranell <76405224+saranell@users.noreply.github.com>
2022-08-23 17:58:09 -0500

    Stuff

--- SchoolPractice/Course.cs
+++ SchoolPractice/Course.cs
@@ -4,10 +4,18 @@ using System.Collections.Generic;
 namespace SchoolPractice
 {
     public class Course
-    {   
+    {
         private string Subject { get; set; }
         private string Teacher { get; set; }
         private List<Student> enrolledStudents;
+
+        public Course(string subject, string teacher, List students)
+        {
+            Subject = subject;
+            Teacher = teacher;
+            Student = students;
         }
+
     }
 
+}
